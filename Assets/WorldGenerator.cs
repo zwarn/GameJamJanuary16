21618502ff2ae978db379947d6bc286fb985ae10
zwarn <Michael.Budahn@gmail.com>
2016-01-09 16:35:26 +0100

    world gen

---
+++ Assets/WorldGenerator.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldGenerator : MonoBehaviour {
+
+    public GameObject Wall_Solid;
+
+	// Use this for initialization
+	void Start () {
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void GenerateWorld ()
+    {
+        
+    }
+}
