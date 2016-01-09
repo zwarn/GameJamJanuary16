e88ce34e69fe2262818575e150a03e131d42b2e6
zwarn <Michael.Budahn@gmail.com>
2016-01-09 18:43:04 +0100

    try to die

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void OnTriggerEnter2D(Collider2D coll) {
+		Debug.Log (coll.gameObject.name);
+		if (coll.gameObject.tag == "Enemy") {
+			Debug.Log("die");
+		}
+	}
+}
